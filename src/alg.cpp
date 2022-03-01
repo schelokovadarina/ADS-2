f5cc62456b9b71bfcd0b84222b9db1acc6a8ef5a
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-03-01 18:50:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,25 +1,25 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
 #include <cmath>
+#include "alg.h"
 
 double pown(double value, uint16_t n) {
 return pow (value, n);
 }
 uint64_t fact(uint16_t n) {
-if (n==0) 
+if ( n==0 ) 
 return 1;
-return n*fact(n-1);
+return n*fact( n-1 );
 }
 double calcItem(double x, uint16_t n) {
 double a=0;
-a=pown(x,n)/fact(n);
+a=pown( x,n )/fact( n );
 return a;
 }
 double expn(double x, uint16_t count) {
 double b=0;
-while (count!=0) {
-b=b+calcItem(x,count);
+while ( count!=0 ) {
+b=b+calcItem( x,count );
 count--;
 }
 b+=1;
@@ -27,8 +27,8 @@ return b;
 }
 double sinn(double x, uint16_t count) {
 double c=0;
-while (count!=1) {
-c=pow(-1, count-1)*calcItem(x,2*count-1);
+while ( count!=1 ) {
+c=pow( -1, count-1 )*calcItem( x,2*count-1 );
 count--; 
 }
 c+=x;
@@ -36,8 +36,8 @@ return c;
 }
 double cosn(double x, uint16_t count) {
 double d=0;
-while (count!=1) {
-d=pow(-1,count-1)*calcItem(x,2*count-2);
+while ( count!=1 ) {
+d=pow( -1,count-1 )*calcItem( x,2*count-2 );
 count--;
 }
 d+=1;
